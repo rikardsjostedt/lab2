5da00eb455c2798dbe471cdcb015e13fe3508f92
rikardsjostedt <rikardsjostedt@users.noreply.github.com>
2019-11-07 14:18:05 +0100

    Documentation and addition of the interface Turbo

--- src/Movable.java
+++ src/Movable.java
@@ -1,6 +1,23 @@
+/**
+ * Methods required for movable objects in order to move.
+ * @author Rikard Sjöstedt
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 public interface Movable
 {
+    /**
+     * Moves the object forward.
+     */
     public void move();
+
+    /**
+     * Turns the object to the left depending on the current direction of the object.
+     */
     public void turnLeft();
+
+    /**
+     * Turns the object to the right depending on the current direction of the object.
+     */
     public void turnRight();
 }
