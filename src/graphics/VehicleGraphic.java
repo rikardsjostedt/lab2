4a4fdbff4c094a7f0cfc48434d8fe19047c2a410
albin <albin.landgren@trinax.se>
2019-11-29 10:26:10 +0100

    Javadoc new methods

--- src/graphics/VehicleGraphic.java
+++ src/graphics/VehicleGraphic.java
@@ -7,6 +7,12 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * Represents a graphical interpretation of a vehicle.
+ * @autor Albin Landgren
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 public class VehicleGraphic {
 	private BufferedImage image;
 	private final Vehicle vehicle;
@@ -14,25 +20,40 @@ public class VehicleGraphic {
 	public VehicleGraphic(Vehicle vehicle, String image) {
 		this.vehicle = vehicle;
 		try {
-			System.out.println(String.format("../pics/%s", image));
 			this.image = ImageIO.read(DrawPanel.class.getResourceAsStream(String.format("../pics/%s", image)));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
+	/**
+	 *
+	 * @return the x attribute of the corresponding vehicle
+	 */
 	public int getX() {
 		return (int) vehicle.getX();
 	}
 
+	/**
+	 *
+	 * @return the y attribute of the corresponding vehicle
+	 */
 	public int getY() {
 		return (int) vehicle.getY();
 	}
 
+	/**
+	 *
+	 * @return the image
+	 */
 	public BufferedImage getImage() {
 		return image;
 	}
 
+	/**
+	 *
+	 * @return the corresponding vehicle
+	 */
 	public Vehicle getVehicle() {
 		return vehicle;
 	}
